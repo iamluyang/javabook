5e60bae6690d9221f5c50379c07b9748d841dfe3
summer.lu <summer.lu@quest.com>
2019-07-05 09:25:15 +0800

    Fixed

--- javabook-collections/src/main/java/com/javabook/collections/Main.java
+++
@@ -1,5 +0,0 @@
-package com.javabook.collections;
-
-public class Main {
-
-}
---
+++ javabook-collections/src/main/java/com/javabook/collections/MainArrayList.java
@@ -0,0 +1,11 @@
+package com.javabook.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainArrayList {
+
+	public static void main(String[] args) {
+		List list = new ArrayList();
+	}
+}
